25c66f7c40479c9e9b3ba8b0f3c3d8af5868e311
Martin Francis <martin@classaxe.com>
2020-02-14 07:16:36 -0500

    0.37.1 More work on CLE

--- src/Controller/Web/Cle.php
+++ src/Controller/Web/Cle.php
@@ -36,10 +36,26 @@ class Cle extends Base
             '_locale' =>    $_locale,
             'mode' =>       $i18n->trans('CLE %NUMBER%', [ '%NUMBER%' => $cle->getCle() ]),
             'system' =>     $system,
-            'cle' =>        $cle
+            'cle' =>        $cle,
+            'reu1' =>       $this->getUrlForRegion($cle, 'Europe', 1),
+            'reu2' =>       $this->getUrlForRegion($cle, 'Europe', 2),
+            'rww1' =>       $this->getUrlForRegion($cle, 'World', 1),
+            'rww2' =>       $this->getUrlForRegion($cle, 'World', 2),
         ];
 
         $parameters = array_merge($parameters, $this->parameters);
         return $this->render('cle/cle.html.twig', $parameters);
+    }
+
+    private function getUrlForRegion($cle, $region, $range) {
+        $params = [];
+        $prefix = "get{$region}Range$range";
+        if ($val = $cle->{ $prefix . 'Low'}()) {
+            $params[] = 'khz_1=' . $val;
+        }
+        if ($val = $cle->{ $prefix . 'High'}()) {
+            $params[] = 'khz_2=' . $val;
+        }
+        return implode('&', $params);
     }
 }
