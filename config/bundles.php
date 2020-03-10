88ca5601d69d06066deec30b0b3f2764fa490f0e
Martin Francis <martin@classaxe.com>
2020-03-10 06:27:02 -0400

    Revert "0.43.0 Begun implementation of Email"
    This reverts commit 7186aea0a5a661ba37de9d54355addebd034b33d.

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
