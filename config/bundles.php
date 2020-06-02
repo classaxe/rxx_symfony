3443d7e7e00f2c527b84e0c98a76e43133449305
Martin Francis <martin@classaxe.com>
2020-06-02 07:21:18 -0400

    2.9.5 Security patching -> Symfony 4.4.9

--- config/bundles.php
+++ config/bundles.php
@@ -14,6 +14,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
