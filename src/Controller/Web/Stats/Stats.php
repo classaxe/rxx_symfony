cabc87fca5601c6d7c97c8d7748e56639de7c6ee
Martin Francis <martin@classaxe.com>
2020-06-01 20:59:33 -0400

    2.9.4 Added more dedicated stats endpoints, linked from Signals Page

--- src/Controller/Web/Stats/Stats.php
+++ src/Controller/Web/Stats/Stats.php
@@ -105,7 +105,122 @@ class Stats extends Base
         $region = ''
     ) {
         $dates =        $this->logRepository->getFirstAndLastLog($system, $region);
-        $textResponse = new Response($dates['last' ] , 200);
+        $textResponse = new Response($dates[ 'last' ] , 200);
+        $textResponse->headers->set('Content-Type', 'text/plain');
+
+        return $textResponse;
+    }
+
+    /**
+     * @Route(
+     *     "/{_locale}/{system}/stats/signals/count/rna",
+     *     requirements={
+     *        "system": "reu|rna|rww"
+     *     },
+     *     name="signals_count_rna"
+     * )
+     * @param $_locale
+     * @param $system
+     * @return Response
+     */
+    public function signals_count_rna (
+        $_locale,
+        $system
+    ) {
+        $stats =        $this->signalRepository->getStats();
+        $textResponse = new Response($stats[ 'signals' ][ 'RNA Only' ] , 200);
+        $textResponse->headers->set('Content-Type', 'text/plain');
+
+        return $textResponse;
+    }
+
+    /**
+     * @Route(
+     *     "/{_locale}/{system}/stats/signals/count/reu",
+     *     requirements={
+     *        "system": "reu|rna|rww"
+     *     },
+     *     name="signals_count_reu"
+     * )
+     * @param $_locale
+     * @param $system
+     * @return Response
+     */
+    public function signals_count_reu (
+        $_locale,
+        $system
+    ) {
+        $stats =        $this->signalRepository->getStats();
+        $textResponse = new Response($stats[ 'signals' ][ 'REU Only' ] , 200);
+        $textResponse->headers->set('Content-Type', 'text/plain');
+
+        return $textResponse;
+    }
+
+    /**
+     * @Route(
+     *     "/{_locale}/{system}/stats/signals/count/rna_reu",
+     *     requirements={
+     *        "system": "reu|rna|rww"
+     *     },
+     *     name="signals_count_rna_reu"
+     * )
+     * @param $_locale
+     * @param $system
+     * @return Response
+     */
+    public function signals_count_rna_reu (
+        $_locale,
+        $system
+    ) {
+        $stats =        $this->signalRepository->getStats();
+        $textResponse = new Response($stats[ 'signals' ][ 'RNA + REU' ] , 200);
+        $textResponse->headers->set('Content-Type', 'text/plain');
+
+        return $textResponse;
+    }
+
+    /**
+     * @Route(
+     *     "/{_locale}/{system}/stats/signals/count/rww",
+     *     requirements={
+     *        "system": "reu|rna|rww"
+     *     },
+     *     name="signals_count_rww"
+     * )
+     * @param $_locale
+     * @param $system
+     * @return Response
+     */
+    public function signals_count_rww (
+        $_locale,
+        $system
+    ) {
+        $stats =        $this->signalRepository->getStats();
+        $textResponse = new Response($stats[ 'signals' ][ 'RWW' ] , 200);
+        $textResponse->headers->set('Content-Type', 'text/plain');
+
+        return $textResponse;
+    }
+
+    /**
+     * @Route(
+     *     "/{_locale}/{system}/stats/signals/count/unlogged",
+     *     requirements={
+     *        "system": "reu|rna|rww"
+     *     },
+     *     name="signals_count_unlogged"
+     * )
+     * @param $_locale
+     * @param $system
+     * @return Response
+     */
+    public function signals_count_unlogged (
+        $_locale,
+        $system
+    ) {
+        $stats =        $this->signalRepository->getStats();
+        $textResponse = new Response($stats[ 'signals' ][ 'Unlogged' ] , 200);
         $textResponse->headers->set('Content-Type', 'text/plain');
 
         return $textResponse;
