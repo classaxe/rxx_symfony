9f2d11d27ac04b5b7f9628b1550ca969b2318fba
Martin Francis <martin@classaxe.com>
2020-07-18 15:07:18 -0400

    2.16.2 Tool Widget redirects added for system and mode only options

--- src/Controller/Web/Tools/Redirect.php
+++ src/Controller/Web/Tools/Redirect.php
@@ -3,6 +3,8 @@ namespace App\Controller\Web\Tools;
 
 use App\Service\GeoService;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -31,6 +33,29 @@ class Redirect extends AbstractController
 
     /**
      * @Route(
+     *     "/tools/{widget}/{args}",
+     *     defaults={"args"=""}
+     * )
+     * @param $widget
+     * @param $args
+     * @param Request $request
+     * @param GeoService $GeoService
+     * @return Response
+     */
+    public function redirect_1b($widget, $args, Request $request, GeoService $GeoService)
+    {
+        $parameters = [
+            '_locale' => $this->get('session')->get('_locale'),
+            'system' => $GeoService->getDefaultSystem(),
+            'widget' => $widget,
+            'args' =>   $args ? $args : $request->query->get('args')
+        ];
+
+        return $this->redirectToRoute('tools_widget', $parameters);
+    }
+
+    /**
+     * @Route(
      *     "/{_locale}/tools",
      *     requirements={
      *        "_locale": "de|en|es|fr",
@@ -52,6 +77,33 @@ class Redirect extends AbstractController
 
     /**
      * @Route(
+     *     "/{_locale}/tools/{widget}/{args}",
+     *     requirements={
+     *        "_locale": "de|en|es|fr",
+     *     },
+     *     defaults={"args"=""}
+     * )
+     * @param $_locale
+     * @param $widget
+     * @param $args
+     * @param Request $request
+     * @param GeoService $GeoService
+     * @return Response
+     */
+    public function redirect_2b($_locale, $widget, $args, Request $request, GeoService $GeoService)
+    {
+        $parameters = [
+            '_locale' => $_locale,
+            'system' => $GeoService->getDefaultSystem(),
+            'widget' => $widget,
+            'args' =>   $args ? $args : $request->query->get('args')
+        ];
+
+        return $this->redirectToRoute('tools_widget', $parameters);
+    }
+
+    /**
+     * @Route(
      *     "/{system}/tools",
      *     requirements={
      *        "system": "reu|rna|rww"
@@ -69,4 +121,28 @@ class Redirect extends AbstractController
 
         return $this->redirectToRoute('tools', $parameters);
     }
+
+    /**
+     * @Route(
+     *     "/{system}/tools/{widget}/{args}",
+     *     defaults={"args"=""}
+     * )
+     * @param $system
+     * @param $widget
+     * @param $args
+     * @param Request $request
+     * @return Response
+     */
+    public function redirect_3b($system, $widget, $args, Request $request)
+    {
+        $parameters = [
+            '_locale' => $this->get('session')->get('_locale'),
+            'system' => $system,
+            'widget' => $widget,
+            'args' =>   $args ? $args : $request->query->get('args')
+        ];
+
+        return $this->redirectToRoute('tools_widget', $parameters);
+    }
+
 }
