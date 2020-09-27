a7b924bd16aa75309239cfa03548a15d36d74e0c
Martin Francis <martin@classaxe.com>
2020-09-27 06:13:59 -0400

    2.20.15 Migration to add index to logs region

---
+++ src/Migrations/Version20200927095646.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200927095646 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('ALTER TABLE `rxx`.`logs`  ADD INDEX `idx_region` (`region` ASC)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('ALTER TABLE `rxx`.`logs`  DROP INDEX `idx_region`');
+    }
+}
