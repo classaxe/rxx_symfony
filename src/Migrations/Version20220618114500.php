d74b20e0723373a07bab465fb8589b4dd67aa619
Martin Francis <martin@classaxe.com>
2022-06-18 11:50:37 -0400

    2.36.1 Migration script to update database to allow for values of 3 and 4 for cle channels (10KHz channel markers)

---
+++ src/Migrations/Version20220618114500.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220618114500 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $sql = <<<EOD
+            ALTER TABLE `rxx`.`cle` 
+                CHANGE COLUMN `date_start` `date_start` DATE NULL DEFAULT NULL,
+                CHANGE COLUMN `date_end`   `date_end`   DATE NULL DEFAULT NULL,
+                CHANGE COLUMN `world_range_1_channels`  `world_range_1_channels`  ENUM('', '1', '2', '3', '4') NULL DEFAULT NULL,
+                CHANGE COLUMN `world_range_2_channels`  `world_range_2_channels`  ENUM('', '1', '2', '3', '4') NULL DEFAULT NULL,
+                CHANGE COLUMN `europe_range_1_channels` `europe_range_1_channels` ENUM('', '1', '2', '3', '4') NULL DEFAULT NULL,
+                CHANGE COLUMN `europe_range_2_channels` `europe_range_2_channels` ENUM('', '1', '2', '3', '4') NULL DEFAULT NULL;
+EOD;
+        $this->addSql($sql);
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $sql = <<<EOD
+            ALTER TABLE `rxx`.`cle` 
+                CHANGE COLUMN `world_range_1_channels`  `world_range_1_channels`  ENUM('', '1', '2') NULL DEFAULT NULL,
+                CHANGE COLUMN `world_range_2_channels`  `world_range_2_channels`  ENUM('', '1', '2') NULL DEFAULT NULL,
+                CHANGE COLUMN `europe_range_1_channels` `europe_range_1_channels` ENUM('', '1', '2') NULL DEFAULT NULL,
+                CHANGE COLUMN `europe_range_2_channels` `europe_range_2_channels` ENUM('', '1', '2') NULL DEFAULT NULL;
+EOD;
+    }
+}
