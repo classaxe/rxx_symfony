cc99472d9a27422d08052cfd42ed94e1d8df0725
Martin Francis <martin@classaxe.com>
2023-08-27 19:56:26 -0400

    2.58.3 Revised public donations page, gifts shown

--- src/Controller/Web/Donate/Donate.php
+++ src/Controller/Web/Donate/Donate.php
@@ -2,13 +2,15 @@
 namespace App\Controller\Web\Donate;
 
 use App\Controller\Web\Base;
+use App\Utils\Rxx;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;  // Required for annotations
 
 class Donate extends Base
 {
-    const MONTHLY_COST =    '$35 CAD';
-    const DOMAIN_COST =     '$14.70 CAD';
+    const MONTHLY_COST =    '55.29';
+    const DOMAIN_COST =     '20.33';
+    const ANNUAL_COST =     (12 * self::MONTHLY_COST) + self::DOMAIN_COST;
     /**
      * @Route(
      *     "/{_locale}/{system}/donate",
@@ -32,9 +34,12 @@ class Donate extends Base
             'system' =>     $system,
             'admins' =>     $admins,
             'domain' =>     static::DOMAIN_COST,
-            'monthly' =>    static::MONTHLY_COST
+            'monthly' =>    static::MONTHLY_COST,
+            'annual' =>     static::ANNUAL_COST,
+            'donations' =>  $this->donationRepository->getDonationsPublic()
         ];
 
+        //print Rxx::y($parameters['donations']); die;
         $parameters = array_merge($parameters, $this->parameters);
         return $this->render('donate/index.html.twig', $parameters);
     }
